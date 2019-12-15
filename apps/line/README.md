878d49d8b86c7bfc4ee5e394a4fadfa0179d558a
Wasin Thonkaew <haxpor@gmail.com>
2019-12-16 04:49:00 +0800

    Add line/ program implemented Bresenham following from tinyrenderer, and wikipedia page

---
+++ apps/line/README.md
@@ -0,0 +1,43 @@
+# Performance comparison
+
+The following is the performance benchmark result running for 1M times to render 3 lines (1 line completely overwrite another one).
+
+You can look at full result at `analysis.txt` for `line3()` function, and `analysis2.txt` for `line7()` function.
+
+## `line3()`
+
+```
+Each sample counts as 0.01 seconds.
+  %   cumulative   self              self     total           
+ time   seconds   seconds    calls  ms/call  ms/call  name    
+ 52.56      1.03     1.03  3000003     0.00     0.00  line3(sr::Vec2i, sr::Vec2i, sr::FrameBuffer&, unsigned int)
+ 23.47      1.49     0.46 204000204     0.00     0.00  sr::FrameBuffer::set(int, int, unsigned int)
+ 14.29      1.77     0.28 204000205     0.00     0.00  std::vector<unsigned int, std::allocator<unsigned int> >::operator[](unsigned long)
+  4.59      1.86     0.09        1    90.02   110.02  sr::FrameBuffer::FrameBuffer(int, int)
+  2.04      1.90     0.04  4000004     0.00     0.00  std::enable_if<std::__and_<std::__not_<std::__is_tuple_like<int> >, std::is_move_constructible<int>, std::is_move_assignable<int> >::value, void>::type std::swap<int>(int&, int&)
+  1.02      1.92     0.02        1    20.00    20.00  std::vector<unsigned int, std::allocator<unsigned int> >::resize(unsigned long, unsigned int const&)
+  1.02      1.94     0.02                             main
+  0.51      1.95     0.01 12000012     0.00     0.00  std::remove_reference<int&>::type&& std::move<int&>(int&)
+  0.26      1.96     0.01  3000003     0.00     0.00  sr::makeColorARGB(unsigned char, unsigned char, unsigned char)
+  0.26      1.96     0.01                             frame_dummy
+  0.00      1.96     0.00  6000006     0.00     0.00  sr::Vec2i::Vec2i(int, int)
+```
+
+## `line7()`
+
+```
+Each sample counts as 0.01 seconds.
+  %   cumulative   self              self     total           
+ time   seconds   seconds    calls  ms/call  ms/call  name    
+ 39.52      0.49     0.49 204000000     0.00     0.00  sr::FrameBuffer::set(int, int, unsigned int)
+ 34.68      0.92     0.43  3000000     0.00     0.00  line7(sr::Vec2i, sr::Vec2i, sr::FrameBuffer&, unsigned int)
+ 18.96      1.16     0.24 204000001     0.00     0.00  std::vector<unsigned int, std::allocator<unsigned int> >::operator[](unsigned long)
+  3.23      1.20     0.04        1    40.01    40.01  std::vector<unsigned int, std::allocator<unsigned int> >::resize(unsigned long, unsigned int const&)
+  1.61      1.22     0.02  3000000     0.00     0.00  sr::makeColorARGB(unsigned char, unsigned char, unsigned char)
+  1.21      1.23     0.02 12000000     0.00     0.00  std::remove_reference<int&>::type&& std::move<int&>(int&)
+  0.81      1.24     0.01                             main
+  0.00      1.24     0.00  6000000     0.00     0.00  sr::Vec2i::Vec2i(int, int)
+  0.00      1.24     0.00  4000000     0.00     0.00  std::enable_if<std::__and_<std::__not_<std::__is_tuple_like<int> >, std::is_move_constructible<int>, std::is_move_assignable<int> >::value, void>::type std::swap<int>(int&, int&)
+```
+
+Tested on Ubuntu 18.04 (5.0.0-37-generic #40~18.04.1-Ubuntu SMP) 8GB RAM, Intel(R) Core(TM) i5-3210M CPU @ 2.50GHz
---
+++ apps/line/main.cpp
@@ -0,0 +1,177 @@
+///
+/// line - line5 from https://github.com/ssloy/tinyrenderer/wiki/Lesson-1:-Bresenham%E2%80%99s-Line-Drawing-Algorithm
+#include "SR_Common.h"
+#include <cmath>
+#include <algorithm>
+
+// define line implementation to choose 1-4
+#define LINE_IMPL 7
+
+// Floating-point operation (multiplication, and rounding), and the choice of constant stepping value
+// are things needed to be improved.
+static void line(const sr::Vec2i& start, const sr::Vec2i& end, sr::FrameBuffer& fb, unsigned int color)
+{
+    for (float t=0.0f; t<1.0f; t+=0.01f)
+    {
+        int x = start.x + (end.x-start.x) * t;
+        int y = start.y + (end.y-start.y) * t;
+        fb.set(x, y, color);
+    }
+}
+
+// more independent computed value of t, and used for both lerping for resultant x, and y
+static void line2(sr::Vec2i start, sr::Vec2i end, sr::FrameBuffer& fb, unsigned int color)
+{
+    for (int x=start.x; x <= end.x; ++x)
+    {
+        float t = (x - start.x) / static_cast<float>(end.x - start.x); 
+        int y = start.y + (end.y - start.y)*t;      //the same as `int y = start.y * (1.0f - t) + end.y*t;`
+        fb.set(x, y, color);
+    }
+}
+
+static void line3(sr::Vec2i start, sr::Vec2i end, sr::FrameBuffer& fb, unsigned int color)
+{
+    bool steep = false;
+    if (std::abs(start.x - end.x) < std::abs(start.y - end.y))          // choose range that has more width
+    {
+        std::swap(start.x, start.y);
+        std::swap(end.x, end.y);
+        steep = true;
+    }
+    if (start.x > end.x)            // make it left-to-right if left is greater than right
+    {
+        std::swap(start.x, end.x);
+        std::swap(start.y, end.y);
+    }
+    for (int x=start.x; x <= end.x; ++x)
+    {
+        float t = (x - start.x) / static_cast<float>(end.x - start.x);
+        int y = start.y + (end.y - start.y)*t;
+        if (steep)
+            fb.set(y, x, color);        // if we swapped the range, then we still need to set pixel at correct position
+        else
+            fb.set(x, y, color);
+    }
+}
+
+static void line4(sr::Vec2i start, sr::Vec2i end, sr::FrameBuffer& fb, unsigned int color)
+{
+    bool steep = false;
+    if (std::abs(start.x - end.x) < std::abs(start.y - end.y))          // choose range that has more width
+    {
+        std::swap(start.x, start.y);
+        std::swap(end.x, end.y);
+        steep = true;
+    }
+    if (start.x > end.x)            // make it left-to-right if left is greater than right
+    {
+        std::swap(start.x, end.x);
+        std::swap(start.y, end.y);
+    }
+    const int dx = end.x - start.x;
+    const int dy = end.y - start.y;
+
+    for (int x=start.x; x <= end.x; ++x)
+    {
+        float t = (x - start.x) / static_cast<float>(end.x - start.x);
+        int y = start.y + (end.y - start.y)*t;
+        if (steep)
+            fb.set(y, x, color);        // if we swapped the range, then we still need to set pixel at correct position
+        else
+            fb.set(x, y, color);
+    }
+}
+
+/// not cover all cases yet i.e. starting point is greater than the ending point
+/// line6 - from https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+static void line6(sr::Vec2i start, sr::Vec2i end, sr::FrameBuffer& fb, unsigned int color)
+{
+    const int dx = end.x - start.x;
+    const int dy = end.y - start.y;
+    int D = 2*dy - dx;      // only determine the sign, then multiply by 2 has no side-effect, as initial value has 1/2, we multiply by 2
+                            // then we need to keep multiply by 2 for the less of the operation for D's related terms
+    int y = start.y;
+
+    for (int x = start.x; x <= end.x; ++x)
+    {
+        fb.set(x, y, color);
+        if (D > 0)
+        {
+            ++y;
+            D = D - 2*dx;
+        }
+        D = D + 2*dy;
+    }
+}
+
+static void line7(sr::Vec2i start, sr::Vec2i end, sr::FrameBuffer& fb, unsigned int color)
+{
+    bool steep = false;
+    if (std::abs(start.x - end.x) < std::abs(start.y - end.y))          // choose range that has more width
+    {
+        std::swap(start.x, start.y);
+        std::swap(end.x, end.y);
+        steep = true;
+    }
+    if (start.x > end.x)
+    {
+        std::swap(start.x, end.x);
+        std::swap(start.y, end.y);
+    }
+    const int dx = end.x - start.x;
+    const int dy = end.y - start.y;
+    int D = 2*dy - dx;      // only determine the sign, then multiply by 2 has no side-effect, as initial value has 1/2, we multiply by 2
+                            // then we need to keep multiply by 2 for the less of the operation for D's related terms
+    int y = start.y;
+
+    for (int x = start.x; x <= end.x; ++x)
+    {
+        if (steep)
+            fb.set(y, x, color);
+        else
+            fb.set(x, y, color);
+
+        if (D > 0)
+        {
+            ++y;
+            D = D - 2*dx;
+        }
+        D = D + 2*dy;
+    }
+}
+
+int main()
+{
+    sr::FrameBuffer fb(100, 100);
+
+#if LINE_IMPL == 1
+    line(sr::Vec2i(13, 20), sr::Vec2i(80, 40), fb, sr::makeColorARGB(255, 255, 255));
+#elif LINE_IMPL == 2
+    // this will produce no 3rd line
+    // 2nd line is not smooth enough
+    line2(sr::Vec2i(13, 20), sr::Vec2i(80, 40), fb, sr::makeColorARGB(255, 255, 255));
+    line2(sr::Vec2i(20, 13), sr::Vec2i(40, 80), fb, sr::makeColorARGB(255, 0, 0));
+    line2(sr::Vec2i(80, 40), sr::Vec2i(13, 20), fb, sr::makeColorARGB(255, 0, 0));
+#elif LINE_IMPL == 3
+    line3(sr::Vec2i(13, 20), sr::Vec2i(80, 40), fb, sr::makeColorARGB(255, 255, 255));
+    line3(sr::Vec2i(20, 13), sr::Vec2i(40, 80), fb, sr::makeColorARGB(255, 0, 0));
+    line3(sr::Vec2i(80, 40), sr::Vec2i(13, 20), fb, sr::makeColorARGB(255, 0, 0));
+#elif LINE_IMPL == 6
+    line6(sr::Vec2i(13, 20), sr::Vec2i(80, 40), fb, sr::makeColorARGB(255, 255, 255));
+    line6(sr::Vec2i(80, 40), sr::Vec2i(13, 20), fb, sr::makeColorARGB(255, 0, 0));      // replace white line, but it should not work
+#elif LINE_IMPL == 7
+    //line7(sr::Vec2i(13, 20), sr::Vec2i(80, 40), fb, sr::makeColorARGB(255, 255, 255));
+    //line7(sr::Vec2i(80, 40), sr::Vec2i(13, 20), fb, sr::makeColorARGB(255, 0, 0));      // replace one line
+    
+    for (int i=0; i<1000000; ++i)
+    {
+    line7(sr::Vec2i(13, 20), sr::Vec2i(80, 40), fb, sr::makeColorARGB(255, 255, 255));
+    line7(sr::Vec2i(20, 13), sr::Vec2i(40, 80), fb, sr::makeColorARGB(255, 0, 0));
+    line7(sr::Vec2i(80, 40), sr::Vec2i(13, 20), fb, sr::makeColorARGB(255, 0, 0));
+    }
+#endif
+
+    sr::TGAImage::write24("out.tga", fb.getFrameBuffer(), fb.getWidth(), fb.getHeight());
+    return 0;
+}
