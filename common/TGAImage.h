haxpor/sr_st

commit cd736f9ff55f2af3961b4cbf035651bba725bde7

Refactor code in common code

common/TGAImage.h
@@ -49,7 +49,7 @@ public:
         }
 
         const int cvtBufferSize = width * height * 3;
-        unsigned char cvtFrameBuffer[cvtBufferSize];
+        unsigned char* cvtFrameBuffer = new unsigned char[cvtBufferSize];
         int cvtI = 0;
         for (int j=0; j<height; ++j)
         {
@@ -68,6 +68,9 @@ public:
             fclose(out_file);
             return false;
         }
+
+        delete[] cvtFrameBuffer;
+        cvtFrameBuffer = nullptr;
 
         // close file
         fclose(out_file);

common/Platform.h
@@ -1,6 +1,6 @@
 #pragma once
 
 #define SR_NAMESPACE_START namespace sr {
-#define SR_NAMESPACE_END };
+#define SR_NAMESPACE_END }
 
 #define SR_MEM_ALIGN(bytes) __attribute__((aligned(bytes)))

commit c1608fc3261f8a1e68462800792a6e9992a1ebd3

Fix write24 of TGAImage. Regression from cd736f9

common/TGAImage.h
@@ -62,7 +62,7 @@ public:
             }
         }
 
-        if (fwrite(cvtFrameBuffer, sizeof(cvtFrameBuffer), 1, out_file) != 1)
+        if (fwrite(cvtFrameBuffer, cvtBufferSize, 1, out_file) != 1)
         {
             std::cerr << "Error writing image data section for .tga file";
             fclose(out_file);