5fd4ddcc6745d6ca1364e08f1da96af72d153f65
Wasin Thonkaew <haxpor@gmail.com>
2020-01-07 16:57:31 +0800

    Add platform macro #define for using sr namespace
    This is for consistency with already had declaration of namespace

--- common/Platform.h
+++ common/Platform.h
@@ -3,4 +3,6 @@
 #define SR_NAMESPACE_START namespace sr {
 #define SR_NAMESPACE_END }
 
+#define SR_NAMESPACE_USING using namespace sr;
+
 #define SR_MEM_ALIGN(bytes) __attribute__((aligned(bytes)))
