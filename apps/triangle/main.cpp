95287fdf583996b572f73bdd6541404a43e96adb
Wasin Thonkaew <haxpor@gmail.com>
2020-01-23 01:58:03 +0800

    Update triangle to include barycentric computation for rasterization of triangle

--- apps/triangle/main.cpp
+++ apps/triangle/main.cpp
@@ -89,6 +89,56 @@ static void triangle2(sr::Vec2i t0, sr::Vec2i t1, sr::Vec2i t2, sr::FrameBuffer&
     }
 }
 
+// barycentric calculation which assume it operates on 2d plane.
+// It accepts only 2d position but internally will automatically supply 0.0f for 3rd dimension.
+// Return barycentric coordinate.
+static sr::Vec3f barycentric(const sr::Vec2i& p1, const sr::Vec2i& p2, const sr::Vec2i& p3, const sr::Vec2i& p)
+{
+    // ref: https://en.wikipedia.org/wiki/Barycentric_coordinate_system at "Conversion between
+    // barycentric and Cartesian coordinates" section in the form of T x alpha = r - r3.
+    // In short, it drills down to finding inverse 2x2 matrix.
+    //
+    // Note that z dimension is simplified to be 0.
+    float det = (p1.x - p3.x) + (p3.x - p2.x);
+    float l1 = ((p.x - p3.x) + (p3.x - p2.x) * p.y) / det;
+    float l2 = ((p.x - p3.x) + (p1.x - p3.x) * p.y) / det;
+    float l3 = 1.0f - l1 - l2;
+    return sr::Vec3f(l1, l2, l3);
+}
+
+// More optimize and different approach in rendering triangle.
+// Assume that input vertices are within the boundary of framebuffer's resolution.
+// This function works with barycentric() function.
+static void triangle3(sr::Vec2i t0, sr::Vec2i t1, sr::Vec2i t2, sr::FrameBuffer& fb, sr::Color32i color)
+{
+    const int maxW = fb.getWidth() - 1;
+    const int maxH = fb.getHeight() - 1;
+
+    // find the bounding box for the triangle
+    sr::Vec2i bbMin(maxW, maxH);
+    sr::Vec2i bbMax(0, 0);
+
+    // find minimum x/y
+    bbMin.x = std::max(0, std::min({bbMin.x, t0.x, t1.x, t2.x}));
+    bbMin.y = std::max(0, std::min({bbMin.y, t0.y, t1.y, t2.y}));
+
+    // find maximum x/y
+    bbMax.x = std::min(maxW, std::max({bbMax.x, t0.x, t1.x, t2.x}));
+    bbMax.y = std::max(maxH, std::max({bbMax.y, t0.y, t1.y, t2.y}));
+
+    sr::Vec2i p;
+    for (p.x = bbMin.x; p.x<=bbMax.x; ++p.x)
+    {
+        for (p.y = bbMin.y; p.y<=bbMax.y; ++p.y)
+        {
+            sr::Vec3f bcScreen = barycentric(t0, t1, t2, p);
+            if (bcScreen.x < 0.0f || bcScreen.y < 0.0f || bcScreen.z < 0.0f)
+                continue;
+            fb.set(p.x, p.y, color.packed);
+        }
+    } 
+}
+
 int main()
 {
     sr::FrameBuffer fb(200, 200);
