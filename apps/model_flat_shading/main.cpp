7a48b426594ab47e6e78b6c2f6e5f2342f7631c2
Wasin Thonkaew <haxpor@gmail.com>
2020-01-23 16:15:25 +0800

    Complete flat shading in apps/model_flat_shading

--- apps/model_flat_shading/main.cpp
+++ apps/model_flat_shading/main.cpp
@@ -7,6 +7,7 @@
 static sr::Color32i white = sr::makeColor32i(255, 255, 255);
 static sr::Color32i green = sr::makeColor32i(0, 255, 0);
 static sr::Color32i red = sr::makeColor32i(255, 0, 0);
+static sr::Vec3f sLightDirection = sr::Vec3f(0.0f, 0.0f, 1.0f);
 
 int main()
 {
@@ -32,15 +33,30 @@ int main()
     {
         auto& face = modelFaces[i];
         sr::Vec2i screenCoords[3];
+        sr::Vec3f worldCoords[3];
 
         // convert from world coordinate to screen coordinate
         for (int j=0; j<3; ++j)
         {
             sr::Vec3f worldCoord = modelVertices[face[j]];
             screenCoords[j] = sr::Vec2i((worldCoord.x + 1.0f) * FB_WIDTH/2.0f, (worldCoord.y + 1.0f) * FB_HEIGHT/2.0f);
+            worldCoords[j] = worldCoord;
         }
-        sr::triangle(screenCoords[0], screenCoords[1], screenCoords[2], fb,
-                sr::Color32i(sr::MathUtil::randInt(255), sr::MathUtil::randInt(255), sr::MathUtil::randInt(255)));
+
+        // assume model is exported with right-hand rule (counter-clockwise) triangle
+        // compute normal vector this face
+        sr::Vec3f faceNormal = sr::cross(worldCoords[1] - worldCoords[0], worldCoords[2] - worldCoords[0]);
+        // normalize normal vector
+        faceNormal.normalize();
+
+        // check lighting effect on this face using dot product
+        float intensity = sr::dot(faceNormal, sLightDirection);
+        if (intensity > 0.0f)
+        {
+            float applyIntensity = intensity * 255;
+            sr::triangle(screenCoords[0], screenCoords[1], screenCoords[2], fb,
+                sr::Color32i(applyIntensity, applyIntensity, applyIntensity));
+        }
     }
 
     sr::TGAImage::write24("out.tga", fb);
